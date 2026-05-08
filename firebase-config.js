7a58833148776e4503ede74ec3bba05d6f3a88b1
Turar1994 <turar.toreniyazov@gmail.com>
2026-05-08 17:32:29 +0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBxETWqa4yW-WmHOaCUC-mez_rBeyZDafA",
-  authDomain: "sparkflow-academy.firebaseapp.com",
-  databaseURL: "https://sparkflow-academy-default-rtdb.firebaseio.com",
-  projectId: "sparkflow-academy",
-  storageBucket: "sparkflow-academy.firebasestorage.app",
-  messagingSenderId: "793574684471",
-  appId: "1:793574684471:web:046da94ddfd19382538cb2"
-};
